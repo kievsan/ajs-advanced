8e43781b49f17736549f8af2105b230b70b86491
Sergey Kievskiy <knhel7@mail.ru>
2023-03-22 01:27:56 +0300

    Webpack edited, 'start' and 'test', 'testcov' added into scripts, code tested and run server successful

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,14 @@
-const path = require('path'); // Node.js стандартный модуль для разрешения путей файлов
-// const webpack = require('webpack'); // для получения доступа к встроенным плагинам
+const { merge } = require('webpack-merge');
 
-module.exports = {
-  entry: './src/index.js', // точка входа
-  output: { // имя выходного файла бандла, и куда его сохранить
-    path: path.resolve(__dirname, 'dist'), // каталог для результатов сборки (записывается в абсолютном формате, для этого исп.resolve(__dirname,)
-    filename: 'main.js', // имя файла с результатами сборки
-  },
-  mode: 'production',
-  // mode: 'development',
-  module: {
+const commonConfig = require('./webpack.common.config.js');
 
-  },
-  plugins: [
+const developmentConfig = require('./webpack.development.config.js');
 
-  ],
+const productionConfig = require('./webpack.production.config.js');
+
+module.exports = (env) => {
+  if (env.development) {
+    return merge(commonConfig, developmentConfig);
+  }
+  return merge(commonConfig, productionConfig);
 };
